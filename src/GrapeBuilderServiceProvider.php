a9997a6786ccfb0b11647362b7cebac9ed3d7b08
Tom v. T <tom@vediansoft.net>
2024-05-03 13:59:02 +0200

    LGB-11: Setup GrapebuilderServiceProvider

---
+++ src/GrapeBuilderServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Vediansoftware\Grapebuilder\ServiceProvider;
+
+use Illuminate\Support\ServiceProvider;
+
+class GrapeBuilderServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+    }
+}
