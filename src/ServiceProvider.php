39faa60fdb6bd2e1e5ba4acba6f8c5a5efc0355c
Tom v. T <tom@vediansoft.net>
2024-05-03 14:40:46 +0200

    LGB-13: altered serviceprovider to match project correctly

---
+++ src/ServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Vediansoftware\Grapebuilder;
+
+use Illuminate\Support\ServiceProvider as IlluminateProvider;
+
+class ServiceProvider extends IlluminateProvider
+{
+    public function register(): void
+    {
+        $this->publishes([
+            $this->root('database/migrations') => database_path('migrations/grapebuilder')
+        ], 'grapebuilder-migrations');
+    }
+
+    public function boot(): void
+    {
+        $this->loadMigrationsFrom(
+            $this->root('database/migrations')
+        );
+    }
+
+    private function src(string $path): string
+    {
+        return __DIR__ . "/{$path}";
+    }
+
+    private function root(string $path): string
+    {
+        return dirname(__DIR__) . "/{$path}";
+    }
+}
